87c0d92ca328b359d1d940ab40e1ec0f552b0b12
ciurte-tudor-freelance <ciurte.tudor.freelance@gmail.com>
2022-07-07 23:12:51 +0000

    Created the Chorisimo FE app

---
+++ apps/chorisimo/fe-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chorisimo-root',
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = 'chorisimo-fe-app';
+}
