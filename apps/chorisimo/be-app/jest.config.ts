e47f005b1073039b0c6a25eafb2539e9653b1b0b
ciurte-tudor-freelance <ciurte.tudor.freelance@gmail.com>
2022-07-07 23:18:23 +0000

    Created the Chorisimo BE app

---
+++ apps/chorisimo/be-app/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'chorisimo-be-app',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/chorisimo/be-app',
+};
